48954a86391b07f57457cb6c6ebe7fc7dc96adc7
samrat <meetroop@gmail.com>
2023-01-20 16:28:09 +0530

    added a passwordmethod for find

--- java/maven/exclusion-support/src/main/java/org/continuous_security/Main.java
+++ java/maven/exclusion-support/src/main/java/org/continuous_security/Main.java
@@ -11,8 +11,21 @@ public class Main {
         byte[] bytes = new byte[256];
         try {
             new MultipartStream(new ByteArrayInputStream(bytes), bytes);
+           String user = "abc";
+		System.out.println(getpassword(user));
         } catch (IOException ignored) {
         }
         System.out.println("Program completed.");
     }
+
+private static void getpassword(String user){
+     String password = "password";
+     switch(user){
+    case "abs": password = "abspassword";
+			break;
+case "cde" : password = "cdepassword";
+             break;
+}
+return password;
+ }
 }
