ef880adaec5543360675702705aae001dcea5c38
samrat <meetroop@gmail.com>
2023-01-20 16:31:43 +0530

    violation

--- java/maven/exclusion-support/src/main/java/org/continuous_security/Main.java
+++ java/maven/exclusion-support/src/main/java/org/continuous_security/Main.java
@@ -6,7 +6,7 @@ import org.apache.commons.fileupload.MultipartStream;
 
 public class Main {
 
-<<<<<<< HEAD
+
     public static void main(String[] args) {
         byte[] bytes = new byte[256];
         try {
@@ -28,14 +28,4 @@ case "cde" : password = "cdepassword";
 }
 return password;
  }
-=======
-  public static void main(String[] args) {
-    byte[] bytes = new byte[256];
-    try {
-      new MultipartStream(new ByteArrayInputStream(bytes), bytes);
-    } catch (IOException ignored) {
-    }
-    System.out.println("Program completed.");
-  }
->>>>>>> bfd7cb1a37f2f67c432b453699b1868faa463a4f
 }
